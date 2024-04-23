e64b19b12b1bae588876d9f10cbe86e3ed152558
vladmih <vladislavs.mihejenkovs@citadele.lv>
2024-04-23 15:42:03 +0300

    Task#1 comments R1.

--- src/main/java/com/taskmaster/taskmasterapp/security/LoginController.java
+++ src/main/java/com/taskmaster/taskmasterapp/security/LoginController.java
@@ -2,8 +2,11 @@ package com.taskmaster.taskmasterapp.security;
 
 import com.taskmaster.taskmasterapp.model.User;
 import com.taskmaster.taskmasterapp.service.UserServiceImpl;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -20,11 +23,18 @@ public class LoginController {
         this.userService = userService;
     }
 
+    //TODO: you are working with MVC so you need to return to client html page.
+    // ResponseEntity is ideal when you need to manipulate with data (json, xml, and etc) but it's not your case. Best way is to refactor method's return entity to String and you will able to return 'home' page with validation message from template
+    // If you want to set additional info to headers, you can use HttpServletResponse as an input argument as you can see below
+
     @PostMapping("/loginSubmit")
-    public ResponseEntity<String> handleLogin(@Valid @ModelAttribute("loginRequest") LoginRequest loginRequest, BindingResult bindingResult) {
+    public ResponseEntity<String> handleLogin(@Valid @ModelAttribute("loginRequest") LoginRequest loginRequest, BindingResult bindingResult, HttpServletResponse response) {
+
 
         //TODO: if user inputs incorrect symbols it should see detailed message and should be returned login page to make another try
         if (bindingResult.hasErrors()) {
+            // for instance
+            response.setHeader("name of your header", "value of your header");
             return ResponseEntity.badRequest().body("Access Denied");
         }
 
