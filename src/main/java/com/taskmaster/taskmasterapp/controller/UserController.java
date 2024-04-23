1e9c82a2e1d6dba5f8b6d548effec06a75aca8c8
Vyacheslav-Pan89 <vyacheslavpan89@gmail.com>
2024-04-23 13:54:57 +0300

    Allot of stuff added

--- src/main/java/com/taskmaster/taskmasterapp/controller/UserController.java
+++ src/main/java/com/taskmaster/taskmasterapp/controller/UserController.java
@@ -1,15 +1,6 @@
 package com.taskmaster.taskmasterapp.controller;
-
-import com.taskmaster.taskmasterapp.model.User;
-import com.taskmaster.taskmasterapp.security.LoginRequest;
 import com.taskmaster.taskmasterapp.service.UserServiceImpl;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,26 +14,5 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@Valid @ModelAttribute("loginRequest") LoginRequest loginRequest, BindingResult bindingResult) {
 
-        if (bindingResult.hasErrors()) {
-            return ResponseEntity.badRequest().body("Access Denied");
-        }
-
-        User user = userService.findUserByName(loginRequest.getUserName());
-        if (user == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .header("Location", "/home")
-                    .body("Invalid Username or Password");
-        }
-
-        if (!user.getPassword().equals(loginRequest.getPassword())) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .header("Location", "/home")
-                    .body("Invalid Username or Password");
-        }
-
-        return ResponseEntity.ok("Login Successful");
-    }
 }
---
+++ src/main/java/com/taskmaster/taskmasterapp/security/LoginController.java
@@ -0,0 +1,45 @@
+package com.taskmaster.taskmasterapp.security;
+
+import com.taskmaster.taskmasterapp.model.User;
+import com.taskmaster.taskmasterapp.service.UserServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LoginController {
+
+    private final UserServiceImpl userService;
+    @Autowired
+    public LoginController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/loginSubmit")
+    public ResponseEntity<String> handleLogin(@Valid @ModelAttribute("loginRequest") LoginRequest loginRequest, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body("Access Denied");
+        }
+
+        User user = userService.findUserByName(loginRequest.getUserName());
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .header("Location", "/home")
+                    .body("Invalid Username or Password");
+        }
+
+        if (!user.getPassword().equals(loginRequest.getPassword())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .header("Location", "/home")
+                    .body("Invalid Username or Password");
+        }
+
+        return ResponseEntity.ok("Login Successful");
+    }
+}
