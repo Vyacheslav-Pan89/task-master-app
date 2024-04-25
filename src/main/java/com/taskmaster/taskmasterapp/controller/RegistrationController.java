8e839f4a67d6d4a70c8d837379e2db51123256b4
Vyacheslav-Pan89 <vyacheslavpan89@gmail.com>
2024-04-25 21:16:24 +0300

    micro changes

--- src/main/java/com/taskmaster/taskmasterapp/controller/RegistrationController.java
+++ src/main/java/com/taskmaster/taskmasterapp/controller/RegistrationController.java
@@ -31,10 +31,12 @@ public class RegistrationController {
 
     @PostMapping("/registrationSubmit")
     public String handleRegistration(@Valid User user, BindingResult bindingResult, Model model) {
+
         if(bindingResult.hasErrors()){
             model.addAttribute("errors", bindingResult.getAllErrors());
             return "registration";
         }
+
         userService.add(user);
         return "redirect:/";
     }
