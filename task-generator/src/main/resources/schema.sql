4f5efd5b1b84d7b78be3ced53413ddb830663e36
BomBomBoss <vladjuha13@gmail.com>
2024-09-18 11:53:16 +0300

    comments #2

--- task-generator/src/main/resources/schema.sql
+++ task-generator/src/main/resources/schema.sql
@@ -5,3 +5,5 @@ CREATE TABLE task (
     description VARCHAR(255),
     taskStatus VARCHAR(255)
 );
+--please add constraints to your table. Now you can enter null fields but title can't be empty. Same for other fields
+-- Also TaskStatus is used as enum so please change type also here
