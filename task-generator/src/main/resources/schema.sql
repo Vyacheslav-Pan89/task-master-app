e8bddb6f3fbca5a1bc1585e603fc887078894e28
Vyacheslav-Pan89 <vyacheslavpan89@gmail.com>
2024-09-18 21:05:57 +0300

    added constraints and changed type to enum

--- task-generator/src/main/resources/schema.sql
+++ task-generator/src/main/resources/schema.sql
@@ -1,9 +1,9 @@
+CREATE TYPE task_status AS ENUM ('NOT_STARTED', 'STARTED', 'COMPLETED');
+
 CREATE TABLE task (
     id BIGINT NOT NULL PRIMARY KEY,
-    title VARCHAR(255),
-    category VARCHAR(255),
-    description VARCHAR(255),
-    taskStatus VARCHAR(255)
+    title VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    taskStatus task_status
 );
---please add constraints to your table. Now you can enter null fields but title can't be empty. Same for other fields
--- Also TaskStatus is used as enum so please change type also here
