a3d99943b07b4174ffcea4a34ad2b2293f47dd76
Vyacheslav-Pan89 <vyacheslavpan89@gmail.com>
2025-03-13 11:55:11 +0200

    todo: continue security configuring

--- web/src/main/java/com/taskmaster/security/SecurityConfig.java
+++ web/src/main/java/com/taskmaster/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.taskmaster.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,9 +12,18 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/static/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/login").permitAll());
         return http.build();
     }
 
